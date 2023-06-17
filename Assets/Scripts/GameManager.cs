e26bf2fd781f1a00108a38c8917f6bf477857a54
Alejoab <aalvarezbo@unal.edu.co>
2023-06-17 10:42:50 -0500

    Added gameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private int _score;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        MainMenu();
+    }
+
+    private void OnDestroy()
+    {
+    }
+
+    public static void MainMenu()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+
+    public void StarGame()
+    {
+        _score = 0;
+        StartCoroutine(LoadGameplayAsyncScene("Gameplay"));
+    }
+
+    private static IEnumerator LoadGameplayAsyncScene(string scene)
+    {
+        var asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        while (!asyncLoad.isDone) yield return null;
+
+        yield return new WaitForSeconds(1f);
+
+        GameEvents.OnStartGameEvent?.Invoke();
+    }
+    
+    public int GetScore()
+    {
+        return _score;
+    }
+}
