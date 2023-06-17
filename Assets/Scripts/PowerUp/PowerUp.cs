d9eb1ad551e7ba4bce758b45093f005125bcf746
Mateo Alvarez L <mateo.alvarez@activeone.co>
2023-06-17 18:16:59 -0500

    Modifies power up animation

--- Assets/Scripts/PowerUp/PowerUp.cs
+++ Assets/Scripts/PowerUp/PowerUp.cs
@@ -1,17 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 public abstract class PowerUp : MonoBehaviour
 {
-    [SerializeField]
-    public float rotationSpeed = 2f;
+    [SerializeField] public float floatingSpeed = 2f;
+
+    [FormerlySerializedAs("_initPos")] [Space(20)] [SerializeField]
+    private Transform _initPosTransform;
+
+    [FormerlySerializedAs("_endPos")] [SerializeField]
+    private Transform _endPosTransform;
     
+    private Vector3 _initPos, _endPos;
+
     private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _initPos = _initPosTransform.position;
+        _endPos = _endPosTransform.position;
+    }
 
     private void Update()
     {
-        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+        float d = (_initPos - _endPos).magnitude;
+        float delta = Mathf.PingPong(Time.time * floatingSpeed, d);
+        transform.position = Vector3.Lerp(_initPos, _endPos, delta / d);
     }
-    
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -21,11 +38,12 @@ public abstract class PowerUp : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if(other.TryGetComponent(out PlayerMovement playerMovement))
+            if (other.TryGetComponent(out PlayerMovement playerMovement))
             {
                 playerMovement.SetPlayerState(PlayerMovement.PlayerState.PowerUp);
                 ActivatePowerUp(other.GetComponent<Player>());
             }
+
             Destroy(gameObject);
         }
         else
@@ -36,6 +54,4 @@ public abstract class PowerUp : MonoBehaviour
     }
 
     protected abstract void ActivatePowerUp(Player player);
-
-
 }
