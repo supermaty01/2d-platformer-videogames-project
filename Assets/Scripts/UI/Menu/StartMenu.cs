cb675094127eee77087a28a54b5a46d5cb2cd0f7
Alejoab <aalvarezbo@unal.edu.co>
2023-06-17 23:37:19 -0500

    Added credits scene

--- Assets/Scripts/UI/Menu/StartMenu.cs
+++ Assets/Scripts/UI/Menu/StartMenu.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    public Button startButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+    }
+
     public void StartGame()
     {
         GameManager.Instance.StarGame();
